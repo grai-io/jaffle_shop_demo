a7aabad90d02f3bc2a6e2ed5c39ca28fe2b47ebf
Ian Eaves <ian.k.eaves@gmail.com>
2023-03-15 23:02:52 -0400

    local_init

--- init.py
+++ init.py
@@ -18,11 +18,11 @@ user = os.environ.get('CLIENT_USER')
 password = os.environ.get('CLIENT_PASSWORD')
 workspace = os.environ.get('CLIENT_WORKSPACE')
 
-client = ClientV1("api.grai.io", "443", workspace=workspace)
-client.set_authentication_headers(user, password)
-
+#client = ClientV1("api.grai.io", "443", workspace=workspace)
+#client.authenticate(username=user, password=password)
+client = ClientV1('localhost', '8000', workspace='test', insecure=True)
+client.authenticate(username='null@grai.io', password='super_secret')
 
 conn = MySQLConnector(**test_credentials)
-res = get_nodes_and_edges(conn, 'v1')
-breakpoint()
-#mysql_update_server(client, **test_credentials)
+nodes, edges = get_nodes_and_edges(conn, 'v1')
+mysql_update_server(client, **test_credentials)
